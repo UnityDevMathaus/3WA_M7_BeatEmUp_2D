314ede088517d6aafab46af39978677138c08191
UnityDevMathaus <mathieu.hoarau@3wa.io>
2022-08-23 16:35:21 +0200

    Ajout du prefab SceneLoader.

---
+++ Assets/Prefabs/ScenesPrefabs/SceneLoader.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private IntVariable _lastScene;
+    [SerializeField] private IntVariable _currentScene;
+
+    public void GoToNextLevel()
+    {
+        //Choix personnel d'aller le chercher via SceneManager plutôt que via le ScriptableObject.
+        int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        nextLevel = (nextLevel >= _lastScene.Value) ? 0 : nextLevel;
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("X - GameOver");
+    }
+
+    public void RetryCurrentLevel()
+    {
+        SceneManager.LoadScene(_currentScene.Value);
+    }
+}
