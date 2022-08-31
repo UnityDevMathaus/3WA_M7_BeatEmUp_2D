a5512d33ed5874f9d5187a3ebbceac3cfe39e11f
UnityDevMathaus <mathieu.hoarau@3wa.io>
2022-08-31 15:34:30 +0200

    Ajout du Prefab Barriers et placement de toutes les barrieres et des checkpoints du Level1.

--- Assets/CameraMovements.cs
+++ Assets/CameraMovements.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class CameraMovements : MonoBehaviour
 {
-
     [SerializeField] private Transform _player1Transform;
+    [SerializeField] private bool _safeZone;
+    [SerializeField] private float _levelLimit = 54.98f;
+    [SerializeField] private float[] _checkpointsPosition
+    = new float[] { -50f, -40f, -29f, -16f, 2f, 23f, 50f };
+    //-50; -40; -29; -16; 2; 23; 50
+
     private Camera _camera;
     private Vector3 _cameraPosition;
+
 
     void Awake()
     {
@@ -17,7 +23,15 @@ public class CameraMovements : MonoBehaviour
     }
     void Update()
     {
-        _cameraPosition.x = _player1Transform.position.x;
-        _camera.transform.position = _cameraPosition;
+        if (CameraCanMove() && _safeZone)
+        {
+            _cameraPosition.x = _player1Transform.position.x;
+            _camera.transform.position = _cameraPosition;
+        }
+    }
+
+    private bool CameraCanMove()
+    {
+        return transform.position.x >= -_levelLimit && transform.position.x <= _levelLimit;
     }
 }
